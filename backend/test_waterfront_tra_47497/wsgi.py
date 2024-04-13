071aaa980ca51dc30426ecc1a1237c4ef624371d
Crowdbotics <team@crowdbotics.com>
2024-04-13 00:08:02 +0000

    Initial commit

---
+++ backend/test_waterfront_tra_47497/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for test_waterfront_tra_47497 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "test_waterfront_tra_47497.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-waterfront-tra-47497",
+  url: "https://test-waterfront-tra-47497.botics.co",
+  api: "https://test-waterfront-tra-47497.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
